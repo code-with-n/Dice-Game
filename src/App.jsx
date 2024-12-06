ed9eb5904c7b8a71dbe92a2b5725688c8ef8daba
code-with-n <ns1nsnsh@gmail.com>
2024-12-06 14:57:25 +0530

    addinng file

--- src/App.jsx
+++ src/App.jsx
@@ -1,7 +1,16 @@
+import { useState } from "react";
 import Pages from "./Dice-Page/pages"
 import Welcome from "./Dice-Page/welcome-page/welcome"
 function App() {
-  <Welcome></Welcome>
+  let [selectTab, setTab] = useState("");
+  const onclickButton =()=>{
+    console.log("the button ..");
+    
+  }
+  return<>
+
+  <Welcome selectTab={selectTab} setTab={setTab}></Welcome>
+  </>
 }
 
 export default App
--- src/Dice-Page/welcome-page/welcome.jsx
+++ src/Dice-Page/welcome-page/welcome.jsx
@@ -1,8 +1,25 @@
-const Welcome = ()=>{
+const Welcome = ({selectTab, setTab})=>{
+    const onclickButton =(e)=>{
+        console
+    }
+    
     return<>
         <div className="welcome">
-            <h4>this is welcome pae</h4>
-            this is welcome page
+          <div className="img-box">
+            
+
+          </div>
+          <div className="start-page">
+            <div className="gameName">
+                <h4>Let's Play Dice Game </h4>
+                <button onClick={()=>onclickButton("Start")} value={start}>Start</button>
+                <button onClick={onclickButton}>Close</button>
+
+
+            </div>
+          </div>
+          
+        
         </div>
     </>
 }
