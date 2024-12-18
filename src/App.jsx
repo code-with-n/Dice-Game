ac49434174f41779ec24527ae88ddcc21c967e40
code-with-n <ns1nsnsh@gmail.com>
2024-12-18 21:58:22 +0530

    added app

--- src/App.jsx
+++ src/App.jsx
@@ -1,13 +1,12 @@
 
 import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
-import './App.css'
+
+// import './App.css'
 
 import Start from './components/startGame/start'
 import PlayGame from './components/playGame/play'
 import StartContextProvider from './store/startContext/startcontext'
-import { use } from 'react'
+// import { use } from 'react'
 
 function App() {
   const [total, setTotal] = useState(0);
